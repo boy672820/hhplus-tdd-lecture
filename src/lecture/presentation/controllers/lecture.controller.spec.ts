81ba05d9ed8f0fe371112c403b3242d0ef9fdaa2
Seonzoo <boy672820@gmail.com>
2024-06-24 22:50:02 +0900

    LectureController.apply() 추가 및 유닛 테스트 작성

--- src/lecture/presentation/controllers/lecture.controller.spec.ts
+++ src/lecture/presentation/controllers/lecture.controller.spec.ts
@@ -1,5 +1,36 @@
+import { Test, TestingModule } from '@nestjs/testing';
 import { LectureController } from './lecture.controller';
+import { ParticipantResponse } from '../responses';
 
 describe('LectureController', () => {
   let lectureController: LectureController;
+
+  beforeEach(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      controllers: [LectureController],
+    }).compile();
+
+    lectureController = moduleRef.get<LectureController>(LectureController);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe('특강 신청하기', () => {
+    it('유저는 특강을 신청할 수 있어야합니다.', async () => {
+      const lectureId = '1';
+      const userId = '1';
+
+      const expected: ParticipantResponse = {
+        realname: '이선주',
+        email: 'boy672820@gmail.com',
+        phone: '01021004364',
+        participantedDate: expect.any(Date),
+      };
+      await expect(
+        lectureController.apply(lectureId, { userId }),
+      ).resolves.toEqual(expected);
+    });
+  });
 });
--- src/lecture/presentation/controllers/lecture.controller.ts
+++ src/lecture/presentation/controllers/lecture.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Param, Post } from '@nestjs/common';
+import { ParticipantResponse } from '../responses';
 
 @Controller('lectures')
-export class LectureController {}
+export class LectureController {
+  @Post(':lectureId/apply')
+  async apply(
+    @Param('lectureId') lectureId: string,
+    { userId }: { userId: string },
+  ): Promise<ParticipantResponse> {
+    return ParticipantResponse.from({
+      realname: '이선주',
+      email: 'boy672820@gmail.com',
+      phone: '01021004364',
+      participantedDate: new Date(),
+    });
+  }
+}
