d066437868bff76db877454c1958d9ef1336c636
Seonzoo <boy672820@gmail.com>
2024-06-24 21:47:39 +0900

    app-config 추가

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { port, validationPipeOptions } from './config';
+import { port, validationPipeOptions } from './app-config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
